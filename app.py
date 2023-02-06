37cd189c4438bb49bbbe7521dceef942e44c7c48
Tiven <vairtor@gmail.com>
2023-02-06 11:08:32 -0300

    Files

--- app.py
+++ app.py
@@ -5,22 +5,11 @@ from View.login import Login
 class App(CTk):
     def __init__(self):
         super().__init__()
-<<<<<<< HEAD
-        self.title('Login')
-        self.resizable(False, False)
-        self.geometry("350x530")
-=======
         self.title('Hybrid')
         self.geometry("350x450")
->>>>>>> b17834391f8e82dbdc8d7a1d8030435ca788377f
 
 
 if __name__ == '__main__':
     app = App()
-<<<<<<< HEAD
-    loginFrame = Login(app)
-=======
     login = Login(app)
->>>>>>> b17834391f8e82dbdc8d7a1d8030435ca788377f
     app.mainloop()
-
