adaa2d7bb3420f16a4726f1e9b5ff28db51b0a8e
Tiven <vairtor@gmail.com>
2023-02-08 12:26:25 -0300

    Change emoji in menu

--- src/View/menu.py
+++ src/View/menu.py
@@ -1,59 +1,59 @@
 from customtkinter import CTkFrame, CTkLabel, CTkButton
 
 if __name__ == "__main__":
-	from customtkinter import CTk
+    from customtkinter import CTk
 
 
 class Menu(CTkFrame):
-	def __init__(self, master=None):
-		super().__init__(master)
+    def __init__(self, master=None):
+        super().__init__(master)
 
-		self.controller = None
+        self.controller = None
 
-		self.collapse = True
-		self.menu_items = []
-		self.menu_labels = []
+        self.collapse = True
+        self.menu_items = []
+        self.menu_labels = []
 
-		self.set_items()
-		self.pack_menu_items()
+        self.set_items()
+        self.pack_menu_items()
 
-	def set_controller(self, controller):
-		self.controller = controller
-		
-	def set_items(self):
-		items = [
-			("Menu", "🚗", self.toggle_menu_collapse),
-			("Home", "🏠", None),
-			("Stock", "🔎", None),
-			("About", "A", None),
-		]
-		for name, icon, comm in items:
-			self.create_menu_item(name, icon, comm)
+    def set_controller(self, controller):
+        self.controller = controller
 
-	def create_menu_item(self, name:str, icon:str, comm):
-		frm = CTkFrame(master=self)
-		i = CTkButton(master=frm, text=icon, command=comm, width=10)
-		n = CTkButton(master=frm, text=name, command=comm)
-		i.pack(padx=10, pady=5, side="left")
-		self.menu_labels.append(n)
-		self.menu_items.append(frm)
+    def set_items(self):
+        items = [
+            ("Menu", "🚗", self.toggle_menu_collapse),
+            ("Home", "🏠", None),
+            ("Stock", "🔎", None),
+            ("About", "👥", None),
+        ]
+        for name, icon, comm in items:
+            self.create_menu_item(name, icon, comm)
 
-	def pack_menu_items(self):
-		for item in self.menu_items:
-			item.pack(padx=10, pady=10)
+    def create_menu_item(self, name: str, icon: str, comm):
+        frm = CTkFrame(master=self)
+        i = CTkButton(master=frm, text=icon, command=comm, width=10)
+        n = CTkButton(master=frm, text=name, command=comm)
+        i.pack(padx=10, pady=5, side="left")
+        self.menu_labels.append(n)
+        self.menu_items.append(frm)
 
-	def toggle_menu_collapse(self):
-		if self.collapse:
-			for label in self.menu_labels:
-				label.pack(padx=10, pady=5, side="right")
-		else: 
-			for label in self.menu_labels:
-				label.pack_forget()
-		self.collapse = not self.collapse
+    def pack_menu_items(self):
+        for item in self.menu_items:
+            item.pack(padx=10, pady=10)
+
+    def toggle_menu_collapse(self):
+        if self.collapse:
+            for label in self.menu_labels:
+                label.pack(padx=10, pady=5, side="right")
+        else:
+            for label in self.menu_labels:
+                label.pack_forget()
+        self.collapse = not self.collapse
 
 
 if __name__ == "__main__":
-	root = CTk()
-	menu = Menu(root)
-	menu.pack()
-	root.mainloop()
+    root = CTk()
+    menu = Menu(root)
+    menu.pack()
+    root.mainloop()
