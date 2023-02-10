5c0cda78e5504a51e09e3a1bbd8a3fb9de4c3c44
Tiven <vairtor@gmail.com>
2023-02-10 01:54:16 -0300

    Change colors, font, and menu size. Add hover event

--- src/View/menu.py
+++ src/View/menu.py
@@ -21,13 +21,18 @@ class Menu(CTkFrame):
 		self.controller = controller
 
 	def set_config(self):
-		self.configure(bg_color="black")
+		self.configure(bg_color="transparent")
 		for item in self.items:
 			item.get("button").configure(
-				fg_color = "black",
+				fg_color = "#D9D9D9",
+				text_color = "#C92C37",
 				corner_radius=0,
-				font = ("Open Sans ExtraBold", 24),
+				font = ("Open Sans Light", 24),
+				hover_color = "#C92C37",
 			)
+			btn = item.get("name")
+			item.get("button").bind("<Enter>",lambda event,arg = btn: self.hover(event,arg))
+			item.get("button").bind("<Leave>",lambda event,arg = btn: self.hover(event,arg))
 
 	def set_items(self):
 		items = (
@@ -49,7 +54,7 @@ class Menu(CTkFrame):
 
 	def pack_items(self):
 		for item in self.items:
-			item.get("button").pack(fill="x")
+			item.get("button").pack(fill="both",ipady=20)
 
 	def toggle_collapse(self):
 		if self.collapse:
@@ -63,6 +68,15 @@ class Menu(CTkFrame):
 	def show_page(self, name_page):
 		self.controller.show_page(name_page)
 
+	def hover(self,event,arg):
+		for item in self.items:
+			if item["name"] == arg:
+				# if event.widget.winfo_class() == "Label": De esta forma conseguimos el widget que triggerea al evento. es otra forma
+				if event.type == "7":
+					print(event.type)
+					item["button"].configure(fg_color="#C92C37",text_color="black")
+				else:
+					item["button"].configure(fg_color="#D9D9D9", text_color="#C92C37")
 
 if __name__ == "__main__":
 	root = CTk()
