8a2da6f73df4ea8950d169d5ab113af16e4767c8
Mateo Astudillo <ast.mateo@gmail.com>
2023-02-12 16:34:15 -0300

    add module Users

--- src/User/Persistence/DBManager.py
+++ src/User/Persistence/DBManager.py
@@ -1,11 +1,10 @@
-# import sqlite3
 from sqlite3 import connect, IntegrityError
 from passlib.hash import sha256_crypt as sha
 
 SALT="paco"
 
 
-class Model:
+class DBManager:
 	def __init__(self, path_db=":memory:"):
 		self.path_db = path_db
 
@@ -20,16 +19,14 @@ class Model:
 			return False
 		return True
 
-	def execute_insert(self, query:str, values:tuple) -> bool:
+	def execute_values(self, query:str, values:tuple) -> bool:
 		try: 
 			conection = connect(self.path_db)
 			cursor = conection.cursor()
 			cursor.execute(query, values)
 			conection.commit()
 			conection.close()
-		except IntegrityError as ex:
-			print(str(ex))
-			print(" username already exist")
+		except IntegrityError:
 			return False
 		except Exception as ex:
 			print(ex)
@@ -45,8 +42,6 @@ class Model:
 		return result
 		
 	def create_table(self):
-		query = "DROP TABLE IF EXISTS users;" ### EXPERIMENTAL
-		self.execute(query)
 		query = "CREATE TABLE IF NOT EXISTS users (\
 			username varchar(64) UNIQUE,\
 			password varchar(64)\
@@ -54,66 +49,56 @@ class Model:
 		self.execute(query)
 
 	def register(self, username:str, password:str):
-		# try: 
 		query = "INSERT INTO users VALUES (?, ?);"
 		password_hashed = self.hash(password)
-		# print("len = ", len(password_hashed), " : ", password_hashed)
-		res = self.execute_insert( query, (username, password_hashed) )
-		# except Exception:
-		# 	print(Exception)
-		# 	return False
-		return res
+		return self.execute_values( query, (username, password_hashed) )
 
 	def login(self, username:str, password:str) -> bool:
 		query = "SELECT * FROM users WHERE username = (?) and password = (?)"
 		password_hashed = self.hash(password)
-		# print("len = ", len(password_hashed), " : ", password_hashed)
 		result = self.execute_select( query, (username, password_hashed) )
 		return result is not None
 
-	def get_all(self):
-		query = "SELECT * FROM users;"
-		return self.execute_select(query, ())
-
 	def remove(self, username:str) -> bool:
 		query = "DELETE FROM users WHERE username = (?)"
-		self.execute(query, username)
-		pass
+		return self.execute(query, username)
 
-	def update(self, username:str = None, password:str = None) -> bool:
-		pass
+	def change_password(self, username:str, password:str) -> bool:
+		password_hashed = self.hash(password)
+		query = "UPDATE users SET password = (?) WHERE username = (?);"
+		return self.execute_values( query, (password_hashed, username) )
+
+	def change_username(self, username:str, new_username:str) -> bool:
+		query = "UPDATE users SET username = (?) WHERE username = (?);"
+		return self.execute_values( query, (new_username, username) )
 
 	def hash(self, password:str) -> str:
 		return sha.using(rounds=1000, salt=SALT).hash(password).split("$")[-1]
 
 
+class User:
+	def __init__(self):
+		self.logged = False
+		self.username = None
+		self.password = None
+
+	def set_username(self, username:str) -> bool:
+		pattern = "[AZa-z0-9]"
+		if not match(pattern, username): 
+			return False
+		self.username = username
+		return True
+
+	def set_password(self, password:str) -> bool:
+		pattern = "[AZa-z0-9]"
+		if not match(pattern, password): 
+			return False
+		self.password = password
+		return True
+
+
 if __name__ == "__main__":
-	from user import User
-	model = Model("user.db")
-	model.create_table()
-	while True:
-		option = input(" 0 - Exit\n 1 - Login\n 2 - Register\n 3 - Delete account\n 4 - Show all\n > ")
-		user = User()
-		if not user.set_username(input(" Username: ")):
-			print(" Invalid username")
-			continue
-		if not user.set_password(input(" Password: ")):
-			print(" Invalid password")
-			continue
-		if int(option) == 0:
-			break
-		elif int(option) == 1:
-			if model.login(user.username, user.password):
-				print(" logged")
-			else:
-				print(" not logged")
-		elif int(option) == 2:
-			model.register(user.username, user.password)
-		elif int(option) == 3:
-			if user.logged:
-				model.remove(user.username, user.password)
-		elif int(option) == 4:
-			print(model.get_all())
-		else:
-			print(" Error")
+	print("DBManager")
+	dbm = DBManager("users.db")
+	dbm.create_table()
 
